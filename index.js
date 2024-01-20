3453d6e93b812f75df552e32319cba585930cea6
assermoussa <asser.y.moussa@gmail.com>
2024-01-19 18:37:45 -0600

    Lab complete

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(target = "roller-skate") {
+    return (`This Saturday, I want to ${target}!`);
+  } 
+  
+  function mondayWork(target = "go to the office") {
+    return (`This Monday, I will ${target}.`);
+  } 
+  
+  function wrapAdjective(symb="%") {
+    return function inner(name = "special") {
+      return (`You are ${symb}${name}${symb}!`);
+    };
+  }
